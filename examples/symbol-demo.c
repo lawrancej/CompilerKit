d5cb7fea9dbce30cd61a02ed15a4f727a2cc1fc3
Robert Cafazzo <robocafaz@gmail.com>
2012-06-18 08:34:54 -0400

    Symbol demo to show Japanese character. (Not sure if working, don't know how to run demo suite.)

--- examples/symbol-demo.c
+++ examples/symbol-demo.c
@@ -7,7 +7,8 @@ int main (int argc, char ** argv)
     CompilerKitSymbol* symbol;
     g_type_init();
     
-    symbol = compilerkit_symbol_new();
+    symbol = compilerkit_symbol_new(21488);
+	fprintf(stdout, "Here's a Japanese character: %s\n", symbol);
     
     /** @todo Briefly show how to use the methods in CompilerKitSymbol to accomplish the task. */
 
