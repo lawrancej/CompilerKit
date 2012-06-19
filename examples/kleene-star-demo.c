e24f0a8f25beb37aaacb597eb7fbd201badc3e56
Joey Lawrance <lawrancej@wit.edu>
2012-06-19 12:39:21 -0400

    Updated all demos to compile.

--- examples/kleene-star-demo.c
+++ examples/kleene-star-demo.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-#include "CompilerKit/kleene-star.h"
+#include "CompilerKit.h"
 
 /** @todo Describe what task main will accomplish. */
 int main (int argc, char ** argv)
 {
-    CompilerKitKleeneStar* kleene_star;
+    GObject *kleene_star;
     g_type_init();
     
-    kleene_star = compilerkit_kleene_star_new();
+    kleene_star = compilerkit_kleene_star_new(compilerkit_symbol_new('a'));
     
     /** @todo Briefly show how to use the methods in CompilerKitKleeneStar to accomplish the task. */
 
