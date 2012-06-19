27c1fbb4d8b7eec2c4ae3c08d0e47ad980c6c4f5
Michael Muesch <mueschm@gmail.com>
2012-06-19 08:30:55 -0400

    Fixed visitor test case

--- tests/visitor-test.c
+++ tests/visitor-test.c
@@ -42,7 +42,88 @@ void test_visitor_method (void)
     g_assert_cmpfloat(g_test_timer_elapsed (), <=, 1);
 }
 
+GObject *print_alternation(CompilerKitVisitor *self, GObject *obj)
+{
+    CompilerKitAlternation *alt;
+    if (!COMPILERKIT_IS_ALTERNATION(obj)) return NULL;
+    
+    alt = COMPILERKIT_ALTERNATION (obj);
+    
+    compilerkit_visitor_visit(self, compilerkit_alternation_get_left (alt));
+    putchar ('|');
+    compilerkit_visitor_visit(self, compilerkit_alternation_get_right (alt));
+    return NULL;
+}
+
+GObject *print_concatenation(CompilerKitVisitor *self, GObject *obj)
+{
+    CompilerKitConcatenation *cat;
+    if (!COMPILERKIT_IS_CONCATENATION(obj)) return NULL;
+    
+    cat = COMPILERKIT_CONCATENATION (obj);
+    
+    compilerkit_visitor_visit(self, compilerkit_concatenation_get_left (cat));
+    compilerkit_visitor_visit(self, compilerkit_concatenation_get_right (cat));
+    return NULL;
+}
+
+GObject *print_kleene_star(CompilerKitVisitor *self, GObject *obj)
+{
+    CompilerKitKleeneStar *star;
+    if (!COMPILERKIT_IS_KLEENE_STAR(obj)) return NULL;
+    
+    star = COMPILERKIT_KLEENE_STAR (obj);
+    
+    putchar('(');
+    compilerkit_visitor_visit(self, compilerkit_kleene_star_get_node (star));
+    printf(")*");
+    return NULL;
+}
+
+GObject *print_empty_set(CompilerKitVisitor *self, GObject *obj)
+{
+    if (!COMPILERKIT_IS_EMPTY_SET(obj)) return NULL;
+    printf("{}");
+    return NULL;
+}
+
+GObject *print_empty_string(CompilerKitVisitor *self, GObject *obj)
+{
+    if (!COMPILERKIT_IS_EMPTY_STRING(obj)) return NULL;
+    printf("\"\"");
+    return NULL;
+}
+
+GObject *print_symbol(CompilerKitVisitor *self, GObject *obj)
+{
+    CompilerKitSymbol *symbol;
+    if (!COMPILERKIT_IS_SYMBOL(obj)) return NULL;
+    
+    symbol = COMPILERKIT_SYMBOL (obj);
+    
+    printf("%c", compilerkit_symbol_get_symbol(symbol));
+    return NULL;
+}
+
+/* Construct a visitor that prints out regular expressions. */
+CompilerKitVisitor* regex_printer ()
+{
+    CompilerKitVisitor *visitor;
+    visitor = compilerkit_visitor_new();
+    
+    compilerkit_visitor_register (visitor, COMPILERKIT_TYPE_ALTERNATION, print_alternation);
+    compilerkit_visitor_register (visitor, COMPILERKIT_TYPE_CONCATENATION, print_concatenation);
+    compilerkit_visitor_register (visitor, COMPILERKIT_TYPE_KLEENE_STAR, print_kleene_star);
+    compilerkit_visitor_register (visitor, COMPILERKIT_TYPE_EMPTY_SET, print_empty_set);
+    compilerkit_visitor_register (visitor, COMPILERKIT_TYPE_EMPTY_STRING, print_empty_string);
+    compilerkit_visitor_register (visitor, COMPILERKIT_TYPE_SYMBOL, print_symbol);
+
+    return visitor;
+}
+
 void test_visitor_null_visit(void)
 {
-	g_assert(compilerkit_visitor_visit(NULL, G_OBJECT(compilerkit_symbol_new('a'))) == NULL);
+	CompilerKitVisitor* visitor = regex_printer();
+	g_assert(compilerkit_visitor_visit(visitor, G_OBJECT(compilerkit_symbol_new('a'))) != NULL);
+	g_assert(compilerkit_visitor_visit(visitor, NULL) != NULL);
 }
