9ebff17abfba1f93cf4d446b8e347e7e7b4a965b
Joey Lawrance <lawrancej@wit.edu>
2012-06-19 08:26:35 -0400

    Improved the testing suite a bit.

--- tests/test-suite.h
+++ tests/test-suite.h
@@ -16,6 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 #include <glib.h>
+#include "CompilerKit.h"
 
 /** Test case function prototypes of the form: void test_class_case (void); */
 void test_FSM_start_state (void);
