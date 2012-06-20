f6bca3ef85bfbb580b7277abca961371ed730b4e
Robert Cafazzo <robocafaz@gmail.com>
2012-06-20 08:37:04 -0400

    Updated symbol-demo.c.

--- examples/symbol-demo.c
+++ examples/symbol-demo.c
@@ -1,14 +1,19 @@
+#include <windows.h>
 #include <stdio.h>
+#include <glib-object.h>
+#include <glib/gunicode.h>
 #include "CompilerKit/symbol.h"
 
 /** @todo Describe what task main will accomplish. */
 int main (int argc, char ** argv)
 {
     CompilerKitSymbol* symbol;
+	
     g_type_init();
     
-    symbol = compilerkit_symbol_new(21488);
-	fprintf(stdout, "Here's a Japanese character: %s\n", symbol);
+	SetConsoleOutputCP(65001);
+    symbol = compilerkit_symbol_new('\u5fb0');
+	printf ("This is a Japanese character: %c \n", compilerkit_symbol_get_symbol(symbol) );
     
     /** @todo Briefly show how to use the methods in CompilerKitSymbol to accomplish the task. */
 
