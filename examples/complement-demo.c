f36ee07344d781aa7c48022be1d7a0114ea8fe3c
Joey Lawrance <lawrancej@wit.edu>
2012-06-22 01:45:25 -0400

    Implemented complement.

--- examples/complement-demo.c
+++ examples/complement-demo.c
@@ -6,7 +6,7 @@ int main (int argc, char ** argv)
     CompilerKitComplement* complement;
     g_type_init();
     
-    complement = compilerkit_complement_new();
+    complement = compilerkit_complement_new(compilerkit_empty_set_get_instance());
     
     /** @todo Briefly show how to use the methods in CompilerKitComplement to accomplish the task. */
 
