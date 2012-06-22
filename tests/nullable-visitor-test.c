807cc0218512588a6eb3df15582fe2f006054458
Joey Lawrance <lawrancej@wit.edu>
2012-06-22 08:05:25 -0400

    Added nullable visitor test.

--- tests/nullable-visitor-test.c
+++ tests/nullable-visitor-test.c
@@ -34,17 +34,49 @@
  * @param None
  * @return void
  */
-void test_nullable_case (void)
+void test_nullable_visitor (void)
 {
     CompilerKitVisitor *nullable;
+    CompilerKitSymbol *symbol;
+    CompilerKitKleeneStar *star;
+    CompilerKitConcatenation *cat;
+    CompilerKitAlternation *alt1, *alt2;
+    
     g_test_message ("Testing Nullable visitor");
     g_test_timer_start ();
     
     /** @todo Test here  */
-    nullable = compilerkit_nullable_visitor();
-    g_assert(FALSE);
+    // symbol is a
+    symbol = compilerkit_symbol_new ('a');
+    // star is a*
+    star = compilerkit_kleene_star_new (symbol);
+    // cat is aa*
+    cat = compilerkit_concatenation_new (symbol, star);
+    // alt1 is emptystring|aa*
+    alt1 = compilerkit_alternation_new (cat, compilerkit_empty_string_get_instance());
+    // alt2 is aa*|a
+    alt2 = compilerkit_alternation_new (cat, symbol);
     
+    nullable = compilerkit_nullable_visitor();
+    
+    // Symbol is not nullable, hence it should return NULL here.
+    g_assert (compilerkit_visitor_visit(nullable, symbol) == NULL);
+    
+    // KleeneStar is nullable, so it should return EmptyString here.
+    g_assert (compilerkit_visitor_visit(nullable, star) == compilerkit_empty_string_get_instance());
+    
+    // Concatenation is not nullable, so it should return NULL here.
+    g_assert (compilerkit_visitor_visit(nullable, cat) == NULL);
+    
+    // Alt1 is nullable, because one side is nullable. 
+    g_assert (compilerkit_visitor_visit(nullable, alt1) == compilerkit_empty_string_get_instance());
+    
+    // Alt2 is not nullable, because neither side is nullable. 
+    g_assert (compilerkit_visitor_visit(nullable, alt2) == NULL);
+
     g_object_unref (nullable);
+    g_object_unref (alt1);
+    g_object_unref (alt2);
 
     // This test shouldn't take too long to run
     g_assert_cmpfloat(g_test_timer_elapsed (), <=, 1);
