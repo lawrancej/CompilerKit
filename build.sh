92a73cf57683ce3e73eb70f4b84326fc84276525
Joey Lawrance <lawrancej@wit.edu>
2012-06-30 16:00:48 -0400

    Added commit leaderboards.

--- build.sh
+++ build.sh
@@ -6,12 +6,14 @@ if [ $# = 0 ]; then
     echo "Usage:    $0 COMMAND [-v]"
     echo ""
     echo "Where COMMAND is one of the following:"
-    echo "build     Builds CompilerKit."
-    echo "rebuild   Removes the existing binaries and rebuilds CompilerKit."
-    echo "clean     Removes the existing binaries."
-    echo "test      Builds and Tests CompilerKit."
-    echo "coverage  Generate test coverage report."
-    echo "board     Generate leaderboard."
+    echo "build       Builds CompilerKit."
+    echo "rebuild     Removes the existing binaries and rebuilds CompilerKit."
+    echo "clean       Removes the existing binaries."
+    echo "test        Builds and Tests CompilerKit."
+    echo "coverage    Generate test coverage report."
+    echo "total       Generate leader board by total line contributions."
+    echo "lastweek    Generate leader board by commits within last week."
+    echo "mergestats  Generate leader board by merges within last week."
     echo ""
     echo "And -v builds verbosely."
     echo ""
@@ -47,10 +49,15 @@ else
         genhtml app.info
         echo "Open build/index.html for the test coverage report."
     fi
-    if [ $1 = "board" ]; then
-        # http://stackoverflow.com/questions/4589731/git-blame-statistics
+    if [ $1 = "total" ]; then
+        # Adapted from: http://stackoverflow.com/questions/4589731/git-blame-statistics
 
-        git ls-tree -r HEAD|sed -E -e 's/^.{53}//'|while read filename; do file "$filename"; done|grep -E ': .*text'|sed -E -e 's/: .*//'|while read filename; do git blame "$filename"; done|sed -E -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//'|sort|uniq -c|sort -nr
-
+        git ls-tree -r HEAD|sed -E -e 's/^.{53}//'|while read filename; do file "$filename"; done|grep -E ': .*text'|sed -E -e 's/: .*//'|while read filename; do git blame -w "$filename"; done|sed -E -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//'|sort|uniq -c|sort -nr
+    fi
+    if [ $1 = "lastweek" ]; then
+        git shortlog --no-merges -s -n --since="(7days)"
+    fi
+    if [ $1 = "mergestats" ]; then
+        git shortlog --merges -s -n --since="(7days)"
     fi
 fi
