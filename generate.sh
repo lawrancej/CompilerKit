e67e21e12301cfd2f5d81ac3db8a5090f80730fc
Joey Lawrance <lawrancej@wit.edu>
2012-07-02 07:07:29 -0400

    Hmm.. may need to rethink generate.sh total target.

--- generate.sh
+++ generate.sh
@@ -47,7 +47,7 @@ next_steps() {
 usage() {
     echo "Build CompilerKit."
     echo ""
-    echo "Usage:    $0 COMMAND [-v]"
+    echo "Usage:      $0 COMMAND [-v]"
     echo ""
     echo "Where COMMAND is one of the following:"
     echo ""
@@ -149,8 +149,12 @@ main() {
     elif [ $1 = "total" ]; then
         # Adapted from: http://stackoverflow.com/questions/4589731/git-blame-statistics
 
+        echo "Non-whitespace lines by author."
+        echo ""
         git ls-tree -r HEAD|sed -E -e 's/^.{53}//'|while read filename; do git blame -w "$filename"; done|sed -E -e 's/.*\((.*)[0-9]{4}-[0-9]{2}-[0-9]{2} .*/\1/' -e 's/ +$//'|sort|uniq -c|sort -nr
     elif [ $1 = "lastweek" ]; then
+        echo "Last week's commits."
+        echo ""
         git shortlog --no-merges -s -n --since="(7days)"
     elif [ $1 = "mergestats" ]; then
         git shortlog --merges -s -n --since="(7days)"
