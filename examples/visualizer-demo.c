7dd06b48110d4f2373a338d94d20225bb174a37b
Robert Cafazzo <robocafaz@gmail.com>
2012-07-03 09:25:14 -0400

    More work.

--- examples/visualizer-demo.c
+++ examples/visualizer-demo.c
@@ -37,11 +37,10 @@ void print_states(CompilerKitFSM *fsm)
     g_list_free (list);
 }
 
-void render (CompilerKitFSM *fsm) {
+void compilerkit_FSM_graphviz_render (CompilerKitFSM *fsm) {
 	int i, j;
 	FILE *fp;
 	GList *states, *transitions;
-	gchar *current;
 
 
 	fp = fopen("visualizer-demo.dot", "w");
@@ -52,7 +51,7 @@ void render (CompilerKitFSM *fsm) {
 	fprintf(fp, ";\n");
 	fprintf(fp, "node [style=solid,shape=doublecircle,fontsize=2];");
 	
-	states = compilerkit_FSM_get_states(fsm);
+	states = compilerkit_FSM_get_accepting_states(fsm);
     while (states) {
 		fprintf(fp, " state_\n");
 		fprintf(fp, (gchar*)states->data );
@@ -62,11 +61,26 @@ void render (CompilerKitFSM *fsm) {
 	fprintf(fp, ";\nnode [shape=circle];\n");
 	printf(" states done\n");
 	
+	/*
+		for (StateCharacter sc : fa.transitions.keySet ()) {
+			for (State s : fa.transitions.get (sc)) {
+				sb.append ("state_");
+				sb.append (sc.s.hashCode ());
+				sb.append (" -> ");
+				sb.append ("state_");
+				sb.append (s.hashCode ());
+				sb.append (" [ label = \"");
+				sb.append ((sc.c != null) ? sc.c : "λ");
+				sb.append ("\" ];\n");
+			}
+		}
+	*/
+	
 	transitions = compilerkit_FSM_get_transitions(fsm);
 	while(transitions)
 	{
-		states = compilerkit_FSM_get_states((CompilerKitFSM*) transitions->data);
-		printf(" transition %c\n", transitions->data);
+		printf(" transition %c\n", (gchar*)(transitions->data));
+		states = g_hash_table_get_keys((GHashTable*)(transitions->data));
 		while(states)
 		{
 			printf(" + to %c\n", states->data);
@@ -76,7 +90,11 @@ void render (CompilerKitFSM *fsm) {
 			fprintf(fp, "state_");
 			fprintf(fp, (gchar*)transitions->data);
 			fprintf(fp, " [ label = \"");
-			if(fsm->priv != NULL)
+			if(states->data != NULL)
+			{
+				fprintf(fp, (gchar*)states->data);
+			}
+			else
 			{
 				fprintf(fp, "λ");
 			}
@@ -85,29 +103,9 @@ void render (CompilerKitFSM *fsm) {
 		}
 		transitions = g_list_next(transitions);
 	}
-	
+
 	fprintf(fp, "}");
 	fclose(fp);
-}
-
-// Match a string.
-void match_string(CompilerKitFSM *fsm)
-{
-    char *state;
-    gchar buf[20];
-    gchar *str;
-
-    printf ("Enter a string: ");
-    scanf ("%20[^\n]s", buf);
-    str = buf;
-    state = compilerkit_FSM_get_start_state(fsm);
-    while (*str) {
-        printf ("%s %c\n", state, *str);
-        state = compilerkit_FSM_get_next_state (fsm, state, *str);
-        *str++;
-    }
-    printf ((compilerkit_FSM_is_accepting_state(fsm, state)) ? 
-             "Accepted!\n" : "Not accepted!\n");
 }
 
 int main (int argc, char ** argv)
@@ -117,7 +115,7 @@ int main (int argc, char ** argv)
     
     fsm = state_machine();
     print_states(fsm);
-    render(fsm);
+    compilerkit_FSM_graphviz_render(fsm);
     
     g_object_unref (fsm);
 }
